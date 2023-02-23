bc4e9d53185bfe3f2fbb5206677f92db94babcf7
Annawhite20 <annawutang80@gmail.com>
2023-02-22 20:36:07 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,20 @@
 #include "main.h"
-/**
- * print_sign - program that print sign of a number 
-*
- * @n: parameter 
-* 
-* Return: 1 if success and 0 otherwise */
-int print_sign(int n)
-{	if (n > 0)
-	{		_putchar('+');		
-return (1);
-	}
-	else if (n == 0)	
-{		_putchar(48);	
-return (0);
-	}	
-else	
-{		_putchar('-'); 
-	}	
-return (-1);
-}
+/
+ * _isalpha-check if character is lowercase or uppercase
+ * @c:character to check if it islowercase.
+ * Return:1-if lowercase/uppercase,0-if not.
+ */
 
+
+
+int _isalpha(int c)
+{
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }
+}
