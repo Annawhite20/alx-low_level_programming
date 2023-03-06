7be27e85e0a02faafe84ab3e08626770e53f2a84
Annawhite20 <annawutang80@gmail.com>
2023-03-06 13:11:35 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,20 @@
 # include "main.h"
-
 /**
  * _memset - Entry point
  * @s: pointed destintion
  * @b: constant byte
  * @n:bytes
+ *
  * Return: Always 0 (Success)
  */
 char *_memset(char *s,char *b, unsigned int n)
  {
-      unsigned int i;
-
+       int i = 0;
+            
       for(i = 0;i < n; i++)
-	      s[i] = b;
+      {     	      s[i] = b;
+                      n--;
+                        
+      }      
       return (s);
  }     
