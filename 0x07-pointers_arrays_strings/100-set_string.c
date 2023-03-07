5e39336abf5973eac3350b953b68ef4a3e72f374
Annawhite20 <annawutang80@gmail.com>
2023-03-06 18:19:42 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer
+ * @s: pointer to change
+ * @to: string
+ * Return: always return o
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
