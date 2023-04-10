4107e6ea63d4f4293b5c1fb62ce269e6ddada10c
Annawhite20 <annawutang80@gmail.com>
2023-04-10 02:31:25 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int -putchar(char c);
+unsigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set bit(unsigned long int n, unsigned int index);
+int clear bit(unsigned long int n, unsigned int indext);
+unsigned int filp_bits(unsigned long int n, unsigned int n);
+int get_endlanness(void)
+
+#endif /* MAIN_H */	
